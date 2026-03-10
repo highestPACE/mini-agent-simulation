1c442ed1a4677b498edd70e66eb4ab74e8d36c16
highestPACE <pschweizer0711@gmail.com>
2026-03-10 12:42:21 +0100

    Implemented World.run()

--- miniAgentSimulation/src/world/World.java
+++ miniAgentSimulation/src/world/World.java
@@ -1,10 +1,21 @@
 package world;
 
-public class World {
+public class World extends Thread {
 	private Grid grid;
 	
 	public World(int height, int width) {
 		setGrid(new Grid(height, width));
+	}
+	
+	@Override
+	public void run() {
+		while (!isInterrupted()) {
+			update();
+		}
+	}
+	
+	public void update() {
+		getGrid().updateAgents();
 	}
 	
 	public void setRandom(int agentsCount, int obstaclesCount) {
