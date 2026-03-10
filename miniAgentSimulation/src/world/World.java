39d0f620ea31dd0a1bfecff696daef408fe6efcc
highestPACE <pschweizer0711@gmail.com>
2026-03-10 13:53:15 +0100

    Added World.getGrid()

--- miniAgentSimulation/src/world/World.java
+++ miniAgentSimulation/src/world/World.java
@@ -1,33 +1,22 @@
 package world;
-import renderer.*;
 
 public class World {
 	private Grid grid;
-	private boolean run = false;
 	
 	public World(int height, int width) {
 		setGrid(new Grid(height, width));
 	}
 	
-	public void run() {
-		run = true;
-		ConsoleRenderer renderer = new ConsoleRenderer();
-		while (run) {
-			update();
-			renderer.render(grid);
-		}
-	}
-	
-	public void stop() {
-		run = false;
-	}
-	
 	public void update() {
-		grid.updateAgents();
+		getGrid().updateAgents();
 	}
 	
 	public void setRandom(int agentsCount, int obstaclesCount) {
-		grid.setRandom(agentsCount, obstaclesCount);
+		getGrid().setRandom(agentsCount, obstaclesCount);
+	}
+	
+	public Grid getGrid() {
+		return grid;
 	}
 
 	public void setGrid(Grid grid) {
