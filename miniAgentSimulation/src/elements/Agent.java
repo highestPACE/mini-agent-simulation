fb19ec782945618f1195fd245061bbf151d77cf8
highestPACE <pschweizer0711@gmail.com>
2026-03-10 14:17:54 +0100

    Agents now have a goal and will not move, when on that goal

--- miniAgentSimulation/src/elements/Agent.java
+++ miniAgentSimulation/src/elements/Agent.java
@@ -6,19 +6,25 @@ public class Agent extends Element {
 	private final Grid grid;
 	private Position position;
 	
-	public Agent(Grid grid, Position position) {
+	private final Goal goal;
+	
+	public Agent(Grid grid, Position agentPos) {
 		this.grid = grid;
-		setPosition(position);
+		setPosition(agentPos);
+		goal = new Goal(grid, grid.getFreePosition());
 	}
 	
 	// TODO ensure that method terminates
 	public Position decide() {
+		if (getPosition().equals(goal.getPosition())) {
+			return getPosition();
+		}
 		Random rand = new Random();
 		Position res = new Position(0, 0);
 		do {
 			res.setX(rand.nextInt(-1, 2) + position.getX());
 			res.setY(rand.nextInt(-1, 2) + position.getY());
-		} while (!grid.isFree(res));
+		} while (!(grid.isFree(res) || res.equals(goal.getPosition())));
 		return res;
 	}
 	
