6a87b158685b22d7c21c88b9cb81cf51d4021d95
highestPACE <pschweizer0711@gmail.com>
2026-03-10 14:17:12 +0100

    Goals now have grid and position

--- miniAgentSimulation/src/elements/Goal.java
+++ miniAgentSimulation/src/elements/Goal.java
@@ -1,7 +1,29 @@
 package elements;
 
-public class Goal extends Element {
+import world.Grid;
+import world.Position;
 
+public class Goal extends Element {
+	private final Grid grid;
+	private Position position;
+	
+	public Goal(Grid grid, Position position) {
+		this.grid = grid;
+		setPosition(position);
+	}
+	
+	public Position getPosition() {
+		return position;
+	}
+
+	public void setPosition(Position position) {
+		if (this.position != null) {
+			grid.setElement(this.position, new Empty());
+		}
+		this.position = position;
+		grid.setElement(this.position, this);
+	}
+	
 	public String getSymbol() {
 		return "G";
 	}
