1600bf4c7f7a7e2a0c8e80ce8969e101a014f065
highestPACE <pschweizer0711@gmail.com>
2026-03-10 13:57:56 +0100

    Main now simulates updated

--- miniAgentSimulation/src/main/Main.java
+++ miniAgentSimulation/src/main/Main.java
@@ -5,17 +5,26 @@ import renderer.*;
 public class Main {
 
 	public static void main(String[] args) {
-		int height = 10;
-		int width = 10;
+		int height = 5;
+		int width = 5;
 		World world = new World(height, width);
 		world.setRandom(1, 5);
-		world.run();
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		
+		System.out.println("Start simulation");
+		ConsoleRenderer renderer = new ConsoleRenderer();
+		renderer.render(world.getGrid());
+		
+		for (int i = 0; i < 5; ++i) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			world.update();
+			renderer.render(world.getGrid());
 		}
-		world.stop();
+		
+		System.out.println("Simulation ended");
 	}
 }
